23a2274776fae8ac5b5d43b2823389f6904b7dff
MelodyneedsMoney <74496976+MelodyneedsMoney@users.noreply.github.com>
2021-04-12 00:49:02 +0800

    Add files via upload

---
+++ js/angler.js
@@ -0,0 +1,145 @@
+
+    $("#lineA1").hide(); $("#lineA2").hide(); $("#lineA3").hide(); $("#lineA4").hide(); $("#lineA5").hide();
+    $("#lineA6").hide(); $("#lineA7").hide(); $("#lineA8").hide(); $("#lineA9").hide(); $("#lineA10").hide();
+    $("#lineA11").hide(); $("#lineA12").hide(); $("#lineA13").hide(); $("#lineA14").hide(); $("#lineA15").hide();
+
+    $("#backA").hide();
+
+    $("#i_an").hover(show1);
+    $("#were").hover(hide);
+
+    function hide(){
+        $("#were").hide();
+    }
+    function show1() {
+    $("#lineA1")
+    .delay(600)
+    .fadeIn(800);
+    $("#i_an").hide();
+    }
+
+    $("#disturbed").hover(show2);
+
+    function show2() {
+    $("#lineA2")
+    .delay(600)
+    .fadeIn(800);
+    $("#disturbed").hide();
+    }
+
+    $("#you").hover(show3);
+    function show3() {
+    $("#lineA3")
+    .delay(600)
+    .fadeIn(800);
+    $("#you").hide();
+    }
+
+    $("#looked").hover(show4);
+    function show4() {
+    $("#lineA4")
+    .delay(600)
+    .fadeIn(800);
+    $("#looked").hide();
+    }
+
+    $("#we").hover(show5);
+    function show5() {
+    $("#lineA5")
+    .delay(600)
+    .fadeIn(800);
+    $("#we").hide();
+    }
+
+    $("#hesitated").hover(show6);
+    function show6() {
+    $("#lineA6")
+    .delay(600)
+    .fadeIn(800);
+    $("#hesitated").hide();
+    }
+
+    $("#she").hover(show7);
+    function show7() {
+    $("#lineA7")
+    .delay(600)
+    .fadeIn(800);
+    $("#she").hide();
+    }
+
+    $("#murmured").hover(show8);
+    function show8() {
+    $("#lineA8")
+    .delay(600)
+    .fadeIn(800);
+    $("#murmured").hide();
+    }
+
+    $("#theboy").hover(show9);
+    function show9() {
+    $("#lineA9")
+    .delay(600)
+    .fadeIn(800);
+    $("#theboy").hide();
+    }
+
+    $("#said").hover(show10);
+    function show10() {
+    $("#lineA10")
+    .delay(600)
+    .fadeIn(800);
+    $("#said").hide();
+    }
+
+    $("#forget").hover(show11);
+    function show11() {
+    $("#lineA11")
+    .delay(600)
+    .fadeIn(800);
+    $("#forget").hide();
+    }
+
+    $("#thepast").hover(show12);
+    function show12() {
+    $("#lineA12")
+    .delay(600)
+    .fadeIn(800);
+    $("#thepast").hide();
+    }
+
+    $("#feel").hover(show13);
+    function show13() {
+    $("#lineA13")
+    .delay(600)
+    .fadeIn(800);
+    $("#feel").hide();
+    }
+
+    $("#sun").hover(show14);
+    function show14() {
+    $("#lineA14")
+    .delay(600)
+    .fadeIn(800);
+    $("#sun").hide();
+    }
+
+    $("#set").hover(show15);
+    function show15() {
+    $("#lineA15")
+    .delay(600)
+    .fadeIn(800);
+    $("#set").hide();
+
+    //let rand=Math.random();
+    $("#backA").delay(1500).fadeIn(100);
+
+    // if (cAngler&&cBoy&&cOwner&&cGoldfish&&cNeighbor){
+    //     document.getElementById("backA").innerHTML="The End";
+    // }
+    // else if (cAngler)
+    // document.getElementById("backA").innerHTML="Continue";
+    // else {document.getElementById("backA").innerHTML="Future";}
+
+    }
+
+
---
+++ js/goldfish.js
@@ -0,0 +1,142 @@
+
+//$("#confess").show();
+
+$("#lineG1").hide(); $("#lineG2").hide(); $("#lineG3").hide(); $("#lineG4").hide(); $("#lineG5").hide();
+$("#lineG6").hide(); $("#lineG7").hide(); $("#lineG8").hide(); $("#lineG9").hide(); $("#lineG10").hide();
+$("#lineG11").hide();$("#lineG12").hide();$("#lineG13").hide();
+
+$("#back1").hide();
+
+$("#confess").hover(show1);
+function show1() {
+$("#lineG1")
+.delay(600)
+.fadeIn(800);
+$("#confess").hide();
+}
+
+$("#to").hover(show2);
+
+function show2() {
+$("#lineG2")
+.delay(600)
+.fadeIn(800);
+$("#to").hide();
+}
+
+$("#me").hover(show3);
+function show3() {
+$("#lineG3")
+.delay(600)
+.fadeIn(800);
+$("#me").hide();
+}
+
+$("#i").hover(show4);
+function show4() {
+$("#lineG4")
+.delay(600)
+.fadeIn(800);
+$("#i").hide();
+}
+
+$("#breathe").hover(show5);
+function show5() {
+$("#lineG5")
+.delay(600)
+.fadeIn(800);
+$("#breathe").hide();
+}
+
+$("#i2").hover(show6);
+function show6() {
+$("#lineG6")
+.delay(600)
+.fadeIn(800);
+$("#i2").hide();
+}
+
+$("#die").hover(show7);
+function show7() {
+
+$("#die").hide();
+}
+
+$("#die2").hover(show8);
+function show8() {
+$("#lineG7")
+.delay(600)
+.fadeIn(800);
+$("#die2").hide();
+}
+
+$("#blood").hover(show9);
+function show9() {
+$("#lineG8")
+.delay(600)
+.fadeIn(800);
+$("#blood").hide();
+}
+
+$("#in").hover(show10);
+function show10() {
+$("#lineG9")
+.delay(600)
+.fadeIn(800);
+$("#in").hide();
+}
+
+$("#water").hover(show11);
+function show11() {
+
+$("#lineG10")
+.delay(600)
+.fadeIn(800);
+$("#water").hide();
+
+
+}
+$("#with").hover(show12);
+function show12() {
+
+$("#lineG11")
+.delay(600)
+.fadeIn(800);
+$("#with").hide();
+
+}
+
+$("#everything").hover(show13);
+function show13() {
+$("#lineG12")
+.delay(600)
+.fadeIn(800);
+$("#everything").hide();
+}
+
+$("#and").hover(show14);
+function show14() {
+$("#lineG13")
+.delay(600)
+.fadeIn(800);
+$("#and").hide();
+}
+
+$("#nothing").hover(show15);
+
+function show15() {
+//let rand=Math.random();
+$("#back1").show();
+
+
+$("#nothing").hide();
+
+// if (cAngler&&cBoy&&cOwner&&cGoldfish&&cNeighbor){
+//     document.getElementById("back1").innerHTML="The End";
+// }
+// else if (cGoldfish)
+// document.getElementById("back1").innerHTML="Continue";
+
+}
+
+
